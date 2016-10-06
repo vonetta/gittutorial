9915489041dc642077ad30cef596d2d8b7ede403
Vonetta <vonettastevenson@gmail.com>
2016-10-06 16:26:05 -0700

    added sass using grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,12 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            sass: {
+                files: 'sass/*.scss',
+                task: ['sass']
+            }
+        },
         concat: {
             options: {
                 separator: "\n\n"
@@ -8,8 +14,11 @@ module.exports = function(grunt) {
             dist: {
                 src: ['js/main.js', 'js/controller.js'],
                 dest: 'bin/<%= pkg.name %>.js'
+            },
+            styles: {
+                src: ['bower_components/boostrap/dist/css/bootstrap.min.css', 'css/custom.css'],
+                dest: 'css/styles.css'
             }
-
         },
 
         uglify: {
@@ -21,12 +30,20 @@ module.exports = function(grunt) {
                     'bin/js/<%= pkg.name %>.min.js': ['bin/<%= pkg.name %>.js']
                 }
             }
+        },
+        sass: {
+            dist: {
+                files: {
+                    'css/custom.css': 'sass/custom.scss'
+                }
+            }
         }
 
     });
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     //task
     grunt.registerTask('default', ['concat', 'uglify']);
 
